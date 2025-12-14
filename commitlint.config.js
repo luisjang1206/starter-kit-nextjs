e5311c9ec95eca3bab93d95ffa373097fe0c6cf7
luis <luis.jang@gmail.com>
2025-12-14 14:29:50 +0900

    🔧 chore: husky pre-commit 및 commitlint 설정 추가

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(?:\p{Emoji_Presentation}|\p{Extended_Pictographic})?\s*(\w+)(?:\((.+)\))?:\s*(.+)$/u,
+      headerCorrespondence: ["type", "scope", "subject"],
+    },
+  },
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "build",
+        "ci",
+      ],
+    ],
+    "subject-case": [0],
+  },
+};
